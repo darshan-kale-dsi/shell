891d3c0a9dd0c750f2605b4698e39cfcab1b04e3
Darshan <darshanlae03@gmail.com>
2024-11-24 10:11:20 -0500

    Updated coworker-changes branch

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -48,17 +48,7 @@ for file in data/raw/*event*.log; do
 done
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
-for file in data/raw/*ipaddr*; do
-  rm "$file"
-done
-
-for file in data/processed/user_logs/*ipaddr*; do
-  rm "$file"
-done
-=======
 rf -rf ./data
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find data/processed -type f > data/inventory.txt
